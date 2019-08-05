df1f3c601cedd763ec246327d3acda608ff19784
jscul <cullen.scott.john@gmail.com>
2019-08-05 11:14:55 -0700

    Updated retrain.py

--- retrain.py
+++ retrain.py
@@ -30,7 +30,35 @@ from tensorflow.python.framework import graph_util
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.util import compat
 
-FLAGS = None
+
+class FLAGS(object):
+    bottleneck_dir = "tf_files/bottlenecks"
+    model_dir = 'tf_files/models/"${ARCHITECTURE}"'
+    output_graph = "tf_files/retrained_graph.pb"
+    output_labels = "tf_files/retrained_labels.txt"
+    image_dir = "tf_files/flower_photos"
+    intermediate_output_graphs_dir = "/tmp/intermediate_graph/"
+    intermediate_store_frequency = 0
+    output_labels = "/tmp/output_labels.txt"
+    summaries_dir = "/tmp/retrain_logs"
+    how_many_training_steps = 4000
+    learning_rate = 0.01
+    testing_percentage = 10
+    validation_percentage = 10
+    eval_step_interval = 50
+    train_batch_size = 100
+    test_batch_size = -1
+    validation_batch_size = 100
+    print_misclassified_test_images = False
+    model_dir = "/tmp/imagenet"
+    bottleneck_dir = "/tmp/bottleneck"
+    final_tensor_name = "final_result"
+    flip_left_right = False
+    random_crop = 0
+    random_scale = 0
+    random_brightness = 0
+    architecture = "inception_v3"
+
 
 # These are all parameters that are tied to the particular model architecture
 # we're using for Inception v3. These include things like tensor names and their
@@ -58,11 +86,13 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
     if not tf.gfile.Exists(image_dir):
         tf.logging.error("Image directory '" + image_dir + "' not found.")
         return None
+
     result = collections.OrderedDict()
     sub_dirs = [
         os.path.join(image_dir, item)
         for item in tf.gfile.ListDirectory(image_dir)
     ]
+
     sub_dirs = sorted(item for item in sub_dirs if tf.gfile.IsDirectory(item))
     for sub_dir in sub_dirs:
         extensions = ["jpg", "jpeg", "JPG", "JPEG"]
@@ -91,13 +121,16 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
         testing_images = []
         validation_images = []
         for file_name in file_list:
+
             base_name = os.path.basename(file_name)
+
             # We want to ignore anything after '_nohash_' in the file name when
             # deciding which set to put an image in, the data set creator has a way of
             # grouping photos that are close variations of each other. For example
             # this is used in the plant disease data set to group multiple pictures of
             # the same leaf.
             hash_name = re.sub(r"_nohash_.*$", "", file_name)
+
             # This looks a bit magical, but we need to decide whether this file should
             # go into the training, testing, or validation sets, and we want to keep
             # existing files in the same set even if more files are subsequently
@@ -108,21 +141,25 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
             hash_name_hashed = hashlib.sha1(
                 compat.as_bytes(hash_name)
             ).hexdigest()
+
             percentage_hash = (
                 int(hash_name_hashed, 16) % (MAX_NUM_IMAGES_PER_CLASS + 1)
             ) * (100.0 / MAX_NUM_IMAGES_PER_CLASS)
+
             if percentage_hash < validation_percentage:
                 validation_images.append(base_name)
             elif percentage_hash < (testing_percentage + validation_percentage):
                 testing_images.append(base_name)
             else:
                 training_images.append(base_name)
+
         result[label_name] = {
             "dir": dir_name,
             "training": training_images,
             "testing": testing_images,
             "validation": validation_images,
         }
+
     return result
 
 
@@ -911,6 +948,7 @@ def save_graph_to_file(sess, graph, graph_file_name):
 
 def prepare_file_system():
     # Setup the directory we'll write summaries to for TensorBoard
+    print(FLAGS)
     if tf.gfile.Exists(FLAGS.summaries_dir):
         tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
     tf.gfile.MakeDirs(FLAGS.summaries_dir)
@@ -965,8 +1003,7 @@ def create_model_info(architecture):
             and version_string != "0.25"
         ):
             tf.logging.error(
-                """"The Mobilenet version should be '1.0', '0.75', '0.50', or '0.25',
-  but found '%s' for architecture '%s'""",
+                "The Mobilenet version should be '1.0', '0.75', '0.50', or '0.25', but found '%s' for architecture '%s'",
                 version_string,
                 architecture,
             )
@@ -979,8 +1016,7 @@ def create_model_info(architecture):
             and size_string != "128"
         ):
             tf.logging.error(
-                """The Mobilenet input size should be '224', '192', '160', or '128',
- but found '%s' for architecture '%s'""",
+                "The Mobilenet input size should be '224', '192', '160', or '128', but found '%s' for architecture '%s'",
                 size_string,
                 architecture,
             )
@@ -1347,189 +1383,5 @@ def main(_):
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--image_dir",
-        type=str,
-        default="",
-        help="Path to folders of labeled images.",
-    )
-    parser.add_argument(
-        "--output_graph",
-        type=str,
-        default="/tmp/output_graph.pb",
-        help="Where to save the trained graph.",
-    )
-    parser.add_argument(
-        "--intermediate_output_graphs_dir",
-        type=str,
-        default="/tmp/intermediate_graph/",
-        help="Where to save the intermediate graphs.",
-    )
-    parser.add_argument(
-        "--intermediate_store_frequency",
-        type=int,
-        default=0,
-        help="""\
-         How many steps to store intermediate graph. If "0" then will not
-         store.\
-      """,
-    )
-    parser.add_argument(
-        "--output_labels",
-        type=str,
-        default="/tmp/output_labels.txt",
-        help="Where to save the trained graph's labels.",
-    )
-    parser.add_argument(
-        "--summaries_dir",
-        type=str,
-        default="/tmp/retrain_logs",
-        help="Where to save summary logs for TensorBoard.",
-    )
-    parser.add_argument(
-        "--how_many_training_steps",
-        type=int,
-        default=4000,
-        help="How many training steps to run before ending.",
-    )
-    parser.add_argument(
-        "--learning_rate",
-        type=float,
-        default=0.01,
-        help="How large a learning rate to use when training.",
-    )
-    parser.add_argument(
-        "--testing_percentage",
-        type=int,
-        default=10,
-        help="What percentage of images to use as a test set.",
-    )
-    parser.add_argument(
-        "--validation_percentage",
-        type=int,
-        default=10,
-        help="What percentage of images to use as a validation set.",
-    )
-    parser.add_argument(
-        "--eval_step_interval",
-        type=int,
-        default=50,
-        help="How often to evaluate the training results.",
-    )
-    parser.add_argument(
-        "--train_batch_size",
-        type=int,
-        default=100,
-        help="How many images to train on at a time.",
-    )
-    parser.add_argument(
-        "--test_batch_size",
-        type=int,
-        default=-1,
-        help="""\
-      How many images to test on. This test set is only used once, to evaluate
-      the final accuracy of the model after training completes.
-      A value of -1 causes the entire test set to be used, which leads to more
-      stable results across runs.\
-      """,
-    )
-    parser.add_argument(
-        "--validation_batch_size",
-        type=int,
-        default=100,
-        help="""\
-      How many images to use in an evaluation batch. This validation set is
-      used much more often than the test set, and is an early indicator of how
-      accurate the model is during training.
-      A value of -1 causes the entire validation set to be used, which leads to
-      more stable results across training iterations, but may be slower on large
-      training sets.\
-      """,
-    )
-    parser.add_argument(
-        "--print_misclassified_test_images",
-        default=False,
-        help="""\
-      Whether to print out a list of all misclassified test images.\
-      """,
-        action="store_true",
-    )
-    parser.add_argument(
-        "--model_dir",
-        type=str,
-        default="/tmp/imagenet",
-        help="""\
-      Path to classify_image_graph_def.pb,
-      imagenet_synset_to_human_label_map.txt, and
-      imagenet_2012_challenge_label_map_proto.pbtxt.\
-      """,
-    )
-    parser.add_argument(
-        "--bottleneck_dir",
-        type=str,
-        default="/tmp/bottleneck",
-        help="Path to cache bottleneck layer values as files.",
-    )
-    parser.add_argument(
-        "--final_tensor_name",
-        type=str,
-        default="final_result",
-        help="""\
-      The name of the output classification layer in the retrained graph.\
-      """,
-    )
-    parser.add_argument(
-        "--flip_left_right",
-        default=False,
-        help="""\
-      Whether to randomly flip half of the training images horizontally.\
-      """,
-        action="store_true",
-    )
-    parser.add_argument(
-        "--random_crop",
-        type=int,
-        default=0,
-        help="""\
-      A percentage determining how much of a margin to randomly crop off the
-      training images.\
-      """,
-    )
-    parser.add_argument(
-        "--random_scale",
-        type=int,
-        default=0,
-        help="""\
-      A percentage determining how much to randomly scale up the size of the
-      training images by.\
-      """,
-    )
-    parser.add_argument(
-        "--random_brightness",
-        type=int,
-        default=0,
-        help="""\
-      A percentage determining how much to randomly multiply the training image
-      input pixels up or down by.\
-      """,
-    )
-    parser.add_argument(
-        "--architecture",
-        type=str,
-        default="inception_v3",
-        help="""\
-      Which model architecture to use. 'inception_v3' is the most accurate, but
-      also the slowest. For faster or smaller models, chose a MobileNet with the
-      form 'mobilenet_<parameter size>_<input_size>[_quantized]'. For example,
-      'mobilenet_1.0_224' will pick a model that is 17 MB in size and takes 224
-      pixel input images, while 'mobilenet_0.25_128_quantized' will choose a much
-      less accurate, but smaller and faster network that's 920 KB on disk and
-      takes 128x128 images. See https://research.googleblog.com/2017/06/mobilenets-open-source-models-for.html
-      for more information on Mobilenet.\
-      """,
-    )
-    FLAGS, unparsed = parser.parse_known_args()
-    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    tf.app.run(main=main)
 
